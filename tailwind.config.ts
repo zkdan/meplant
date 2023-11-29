e3423dbe86505a0e7cbdf5045f712431e85f4c99
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-11-28 20:11:57 -0500

    adding tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,30 @@
-import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: [
-    './src/Panes/*.tsx',
-    './src/Layout/*.tsx',
-    './index.html',
-    './node_modules/flowbite/**/*.js'
+    "./src/Panes/*.tsx",
+    "./src/Layout/*.tsx",
+    "./index.html",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     colors: {
-    transparent: 'transparent',
-    current: 'currentColor',
-    black: colors.black,
-    white: colors.white,
-    gray: colors.gray,
-    slate:colors.slate,
-    emerald: colors.emerald,
-    indigo: colors.indigo,
-    yellow: colors.yellow,
-    lightSlate:'#3a3a3a'
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      slate: colors.slate,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      lightSlate: "#3a3a3a",
+    },
+    fontSize: {
+      base: "1.8rem",
+    },
+    extend: {},
   },
-  fontSize:{
-    base:'1.8rem'
-  },
-  extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-} satisfies Config
-
+  plugins: [require("flowbite/plugin")],
+} satisfies Config;
